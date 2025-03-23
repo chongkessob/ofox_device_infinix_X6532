93d183fd68870a4aadb272cade723d097fa487a1
chongkessob <chonks091004@gmail.com>
2025-03-22 20:28:18 -0700

    remove fastbootd line

--- TW_flags.mk
+++ TW_flags.mk
@@ -11,7 +11,6 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_INCLUDE_FASTBOOTD := true
 TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
 RECOVERY_SDCARD_ON_DATA := true
